bae28161a2cacffa0044ab41900f827035222099
Joao Antunes <jasantunes@gmail.com>
2017-01-02 14:40:28 -0800

    email should be saved as lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false } # simple: uniqueness: true      
+                    uniqueness: { case_sensitive: false } # simple: uniqueness: true
+     
+  # Ensure uniqueness by downcasting the email attribute
+  before_save do
+    self.email = email.downcase
+  end
+      
 end
