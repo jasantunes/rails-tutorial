6a399ec1432bb534f8008f7702ca6502eec5c029
Joao Antunes <jasantunes@gmail.com>
2017-01-02 18:08:04 -0800

    test invalid signup information does not add user

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,4 +8,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Sign up")
   end
   
+  test "invalid signup information does not add user" do
+    # assert_no_difference is more idiomatically correct than:
+    #   before_count = User.count
+    #   post users_path, ...
+    #   after_count  = User.count
+    #   assert_equal before_count, after_count
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+  end
 end
