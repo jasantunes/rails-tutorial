cae242a35b1b47722f55c1dc232addac7c932a7f
Joao Antunes <jasantunes@gmail.com>
2017-01-02 18:37:58 -0800

    handle successful signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,4 +21,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
   end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "foobar123",
+                                         password_confirmation: "foobar123" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?   # or assert_select "div.alert-success"
+    
+  end
+
 end
