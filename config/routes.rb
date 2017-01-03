f90c6991c0c721874462ec7e207d5d19082fb05d
Joao Antunes <jasantunes@gmail.com>
2017-01-02 16:20:17 -0800

    add resource Users and implement action show

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new' 
+  get  '/signup',  to: 'users#new'
+  
+  # Adds all the actions needed for a RESTful Users resource:
+  # GET, POST, PATCH, and UPDATE to /users, /users/new, /users/<id>, /users/<id>/edit, etc.
+  resources :users 
 end
