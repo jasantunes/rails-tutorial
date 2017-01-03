6fecd5447595487de5d916276be3a6ed6c921e68
Joao Antunes <jasantunes@gmail.com>
2017-01-02 18:15:12 -0800

    change route for create to /signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  
+  # Customize the URLs for both an unsubmitted signup form
+  # and for a submitted signup form to be /signup.
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   
   # Adds all the actions needed for a RESTful Users resource:
   # GET, POST, PATCH, and UPDATE to /users, /users/new, /users/<id>, /users/<id>/edit, etc.
