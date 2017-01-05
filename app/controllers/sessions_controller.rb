68df0e0ba639f335828bddb8b72633d57e4b14f4
Joao Antunes <jasantunes@gmail.com>
2017-01-04 21:34:39 -0800

    customize Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      puts "User " + params[:session][:email] + " logged in"
+    else
+      # Create an error message.
+      puts "User " + params[:session][:email] + " not logged in"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
+  # Static Pages controller.  
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   
-  # Customize the URLs for both an unsubmitted signup form
-  # and for a submitted signup form to be /signup.
+  # Users controller.  
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  # Sessions controller.  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   # Adds all the actions needed for a RESTful Users resource:
   # GET, POST, PATCH, and UPDATE to /users, /users/new, /users/<id>, /users/<id>/edit, etc.
